1f90a298927be7573814e3395771a84c41345430
amri-03 <amanrkumar030@gmail.com>
2026-05-02 04:14:52 +0530

    Update test-api.py

--- price-tracker/test-api.py
+++ price-tracker/test-api.py
@@ -1,12 +1,15 @@
 import requests
 
+# Requests data from the server using the request library's get method
 response = requests.get("https://api.github.com/users/amri-03")
 
 print("Status Code: ", response.status_code)
 print("Content Type: ", response.headers["Content-Type"])
 
+# Converts the raw response into structured json file
 data = response.json()
 
+# Avoids error when the user does not exist
 if response.status_code == 200:
     print("Username: ", data["login"])
     print("Public Repos: ", data["public_repos"])
