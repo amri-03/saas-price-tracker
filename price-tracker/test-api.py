29342effa2364c06c5d959226a3aed3a1732769e
Aman Kumar <117673874+amri-03@users.noreply.github.com>
2026-05-08 19:00:21 +0530

    Update API request logic and error handling
    Refactor API request handling and improve error checking.

--- price-tracker/test-api.py
+++ price-tracker/test-api.py
@@ -1,7 +1,9 @@
 import requests
 
+url = "https://api.github.com/users/amri-03"
+
 # Requests data from the server using the request library's get method
-response = requests.get("https://api.github.com/users/amri-03")
+response = requests.get(url)
 
 print("Status Code: ", response.status_code)
 print("Content Type: ", response.headers["Content-Type"])
@@ -10,7 +12,9 @@ print("Content Type: ", response.headers["Content-Type"])
 data = response.json()
 
 # Avoids error when the user does not exist
-if response.status_code == 200:
+if response.status_code != 200:
+    print(f"Failed to fetch data.")
+else:
     print("Username: ", data["login"])
     print("Public Repos: ", data["public_repos"])
     print("Account Created: ", data["created_at"])
